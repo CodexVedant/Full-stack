5c502049bcb811ee46371bc9071490a4a8257add
CodexVedant <jayswalvedant45@gmail.com>
2025-12-19 13:09:25 +0530

    var file

---
+++ JS/basic/data_type.js
@@ -0,0 +1,4 @@
+//there are two type of data types primitive and non primitive 
+//primitive data types = number, string, boolean, null, undefined, symbol, bigint
+//non primitive data types = object, array, function ,date, map and set
+console.log("hello")
--- JS/basic/variable.js
+++ JS/basic/variable.js
@@ -1,2 +1,14 @@
-//hello 
-var a = 10;
+//variable declaration 
+
+var hello = 10;
+var hello12 = 10;
+var _helo = 10;
+
+console.log(hello,_helo,hello12);
+
+//not right way for variable declaration
+
+var @helo = 10; 
+var hello@12 = 10;
+var 123hello = 10; 
+console.log(123hello,@helo,,hello@12)
